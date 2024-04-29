14c886f63d6af88135bd848d209cb39930821421
shehan1215 <kaushikashehan123@gmail.com>
2024-04-29 15:32:07 +0530

    created the e-commerce web site using MERN stack

---
+++ src/page/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-4xl md:text-7xl font-bold py-3 text-red-600 text-center'>About Us</h1>
+      <p>Welcome to ShenX E-Shopping!
+        we are passionate about providing you 
+        with an exceptional online shopping experience. With a wide range 
+        of products, seamless browsing, and secure transactions, we are 
+        dedicated to making your shopping experience convenient, enjoyable, 
+        and secure.</p>
+    </div>
+  )
+}
+
+export default About
